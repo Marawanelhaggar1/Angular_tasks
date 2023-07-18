674cb5ce723a4b5fe4f762e6d9cd44c7a02f9c08
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-07-18 12:42:54 +0300

    1

--- lec2-task/src/app/products/product/product.component.ts
+++ lec2-task/src/app/products/product/product.component.ts
@@ -1,25 +1,27 @@
-import { Component, Injectable, Input } from "@angular/core";
-import {Product} from "../models/productModel";
+import { Component, Injectable, Input } from '@angular/core';
+import { Product } from '../models/productModel';
 
-
-@Component(
-    {
-        selector: "productItem-root",
-        templateUrl: "./product.component.html",
-        styleUrls: ["./product.component.scss"]
-    }
-    )
+@Component({
+  selector: 'productItem-root',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
 export class ProductItemComponent {
-    @Input() productItem: Product;
-    @Input() products!: Product[];
-    constructor(){
-        this.productItem = new Product();
-    }
+  @Input() productItem: Product;
+  @Input() products!: Product[];
+  @Input() filter: String = 'All';
 
-    largerThan100(product:Product){
-      return product.priceProduct>100
-    }
+  constructor() {
+    this.productItem = new Product();
+  }
 
-
+  largerThan100(product: Product) {
+    return product.priceProduct > 100;
+  }
+  shouldBeViewed(product: Product): boolean {
+    if (this.filter == 'All') return true;
+    if (this.filter == 'Avaliable' && product.isAvailable) return true;
+    if (this.filter == 'Featured' && product.isFeatured) return true;
+    return false;
+  }
 }
-
